d5ebcb5ee1b4eb40e85b38fb47c52542b941b129
tran_y <davy.tran@epitech.eu>
2015-03-16 01:18:39 +0100

    add dynamic image

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -8,11 +8,13 @@
 	var gems = [
 		{
 			name: 'Xperia Z2',
-			url: 'xperia-z2.html'
+			url: 'xperia-z2.html',
+			wallpaper: 'assets/images/wallpaper-000.jpg'
 		},
 		{
 			name: 'Xperia Z1',
-			url: 'xperia-z1.html'
+			url: 'xperia-z1.html',
+			wallpaper: 'assets/images/wallpaper-001.jpg'
 		}
 	];
 })();
